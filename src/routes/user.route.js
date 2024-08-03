bc9e0f9eb59d0b7e29bde22b7f03bdf1c2f09714
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-08-03 18:30:34 +0530

    ds

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import {upload} from "../middlewares/multer.middleware.js"
-import {Registeruser,  loginUser,logoutUser,refreshAccessToken}  from "../controllers/user.controller.js"
 import verifyJWT from "../middlewares/auth.middleware.js";
+import {Registeruser,  loginUser,logoutUser,refreshAccessToken}  from "../controllers/user.controller.js"
 
 const router = Router();
 router.route("/register").post(
