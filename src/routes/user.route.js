6b652e52f13bbe3c89597865c322f3d65e85f9cb
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-08-11 20:30:02 +0530

    router config

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,27 +1,35 @@
 import { Router } from "express";
-import {upload} from "../middlewares/multer.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js"
 import verifyJWT from "../middlewares/auth.middleware.js";
-import {Registeruser,  loginUser,logoutUser,refreshAccessToken}  from "../controllers/user.controller.js"
+import { Registeruser, changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, updateUserAvatar, updateUserCoverimage,getCurrentUser,getUserChannelProfile,getWatchHistory,updateUserAvatar,updateUserCoverimage,updateAccountDetail } from "../controllers/user.controller.js"
+import { verify } from "jsonwebtoken";
 
 const router = Router();
 router.route("/register").post(
     upload.fields([
-    {
-        name : "avatar",
-        maxCount: 1
-    }, {
-        name :"coverimage",
-        maxCount : 1
-    }
+        {
+            name: "avatar",
+            maxCount: 1
+        }, {
+            name: "coverimage",
+            maxCount: 1
+        }
     ]),
     Registeruser
-    )
-
+)
 router.route("/login").post(loginUser);
 
 //secured routes
 
-router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-details").patch(verifyJWT) //update selected by patch
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverimage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 export default router;
 
