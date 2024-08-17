163e4c326a433519cfe0366d39be527db0bc4b2a
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-08-17 19:56:00 +0530

    Tweet controller complete

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -29,7 +29,24 @@ const createTweet = asynchandler(async (req, res) => {
 })
 
 const getUserTweets = asynchandler(async (req, res) => {
-   
+   const {userid} = req.params
+   if(!isValidObjectId(userid)){
+    throw new Apierror(400,"Invalid user id")
+   }
+   if(!user){
+    throw new Apierror(400,"User does not exist")
+ }
+
+ const user = await User.findById(userid)
+ const alltweet = await Tweet.find({
+    owner :userid
+ })
+ if(alltweets.length===0){
+    throw new Apierror(400,"No tweets by the user")
+ }
+ return res.status(200)
+     .json(new Apisuccess(200,"All tweets fetched successfully",{alltweets}))
+
 })
 
 const updateTweet = asynchandler(async (req, res) => {
