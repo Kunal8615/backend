4f3a0b71c6ba1d9e06c97247b44dfb6c2dcfa8db
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-08-23 18:04:07 +0530

    define dashboard route

---
+++ src/routes/dashboard.route.js
@@ -0,0 +1,15 @@
+import verifyJWT from "../middlewares/auth.middleware";
+import { Router } from "express";
+import {  getChannelStats, 
+    getChannelVideos} from "../controllers/dashboard.controller.js"
+
+
+const router = Router()
+
+
+router.use(verifyJWT)
+
+router.route("/stats/:channelid").get(getChannelStats)
+router.route("/videos/:channelid").get(getChannelVideos);
+
+export default router
